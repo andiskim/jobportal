0073fee7d555f28ce6c6906e35663ccdd8ce0620
Andy <andiskim@gmail.com>
2016-04-28 22:34:15 +0900

    save before useraccounts:materialize

---
+++ client/main.js
@@ -0,0 +1,39 @@
+// Event for register form
+Template.register.events({
+  'submit form': function(event) {
+    event.preventDefault();
+    var email = $('[name=email]').val();
+    var password = $('[name=password]').val();
+    Accounts.createUser({
+      email: email,
+      password: password
+    }, function(error) {
+      if(error) {
+        console.log(error.reason)
+      } else {
+          Router.go("home");
+      }
+    });
+    Router.go('home');
+  }
+});
+
+//Login events
+Template.login.events({
+  'submit form': function(event) {
+    event.preventDefault();
+    var email = $('[name=email]').val();
+    var password = $('[name=password]').val();
+    Meteor.loginWithPassword(email, password, function(error) {
+      if(error) {
+        console.log(error.reason);
+      } else {
+        var currentRoute = Router.current().route.getName();
+        console.log(currentRoute);
+        if(currentRoute == "login") {
+          Router.go("home");
+        }
+      }
+    });
+  }
+});
